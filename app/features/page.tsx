c431be8bde26fb7845f672e570a13acd9406d386
flagrantii <tanuson679@gmail.com>
2023-11-07 01:48:28 +0700

    Add Navbar and Footer components, update dependencies

---
+++ app/features/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Features = () => {
+  return (
+    <div>
+        <p>
+            hi
+        </p>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            
+        </ul>
+    </div>
+  )
+}
+
+export default Features
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <Navbar />
+        {children}
+      <Footer/>
+    </>
   )
 }
